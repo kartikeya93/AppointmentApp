2a1ef9ed1f374ed79b85c9a97a247f14ce029203
Kartikeya  Singh <kartikeyasingh@Kartikeyas-MacBook-Pro.local>
2020-11-19 18:17:04 -0500

    added update - auth pending

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -35,4 +35,31 @@ module.exports = {
       })
       .catch((error) => res.status(400).send(error));
   },
+  update(req, res) {
+    return User.findByPk(req.params.id)
+      .then((user) => {
+        if (!user) {
+          return res.status(404).send({
+            message: "User Not Found",
+          });
+        }
+        return user
+          .update({
+            firstName: req.body.firstName || user.firstName,
+            lastName: req.body.lastName || user.lastName,
+            email: req.body.email || user.email,
+            phone: req.body.phone || user.phone,
+            password: req.body.password || user.password,
+            dateOfBirth: req.body.dateOfBirth || user.dateOfBirth,
+            gender: req.body.gender || user.gender,
+            fitnessGoal: req.body.fitnessGoal || user.fitnessGoal,
+            currentFitness: req.body.currentFitness || user.currentFitness,
+            height: req.body.height || user.height,
+            weight: req.body.weight || user.weight,
+          })
+          .then(() => res.status(200).send(user)) // Send back the updated todo.
+          .catch((error) => res.status(400).send(error));
+      })
+      .catch((error) => res.status(400).send(error));
+  },
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -10,4 +10,5 @@ module.exports = (app) => {
   app.post("/api/users", usersController.create);
   app.get("/api/users", usersController.list);
   app.get("/api/users/:id", usersController.retrieve);
+  app.put("/api/users/:id", usersController.update);
 };
